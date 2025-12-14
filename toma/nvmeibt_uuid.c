d6166824fca1788a714a0cad5142e3883cdf36f1
danielhsh <DanielHe@nvidia.com>
2025-12-14 13:16:31 +0200

    Toma: refactor uuid
    Change-Id: Id3cd81a6457e9103705c124d0d515619adfa1a2f
    Signed-off-by: danielhsh <DanielHe@nvidia.com>

--- toma/nvmeibt_uuid.c
+++ toma/nvmeibt_uuid.c
@@ -1,10 +1,3 @@
-/*
- * nvmeibt_uuid.c
- *
- *  Created on: Aug 26, 2020
- *      Author: yair
- */
-
 #include <time.h>
 #include <stdlib.h>
 #include <stdint.h>
@@ -26,13 +19,11 @@ const union nvmeib_uuid nvmeib_uuid_null_val = {.ll = {0, 0}};
 // bytes:     c0 84 2d 53 9a db ea 11  a4 83 d5 b0 31 58 bd 81
 // LE Qwords: { 0x11eadb9a532d84c0ULL, 0x81bd5831b0d583a4ULL }
 // TODO: include and use "nvmeib_uuid_be.h"
-int nvmeibt_urn_uuid_str_to_union_uuid(union nvmeib_uuid *uuid, const char *urn_uuid_str)
-{
+int nvmeibt_urn_uuid_str_to_union_uuid(union nvmeib_uuid *uuid, const char *urn_uuid_str) {
 	int i,j, k = 0;
 	int	rv = 0;
 	union _uuid_swapper *u = (union _uuid_swapper *)uuid;
-	const char	*urn_uuid_ptr = urn_uuid_str;
-
+	const char *urn_uuid_ptr = urn_uuid_str;
 	for (i=0; i<2; i++) {
 		uint64_t v = 0;
 		for (j=0; j<16; j++) {
@@ -41,9 +32,7 @@ int nvmeibt_urn_uuid_str_to_union_uuid(union nvmeib_uuid *uuid, const char *urn_
 
 			if (c == '-') {
 				if (k != (8 + 1) && k != (13 + 1) && k != (18 + 1) && k != (23 + 1)) {
-#ifdef N_Ef
-				N_Ef(vxrwh81, "Illegal uuid=@STR k=@INT c=@CHAR", urn_uuid_str, k, c);
-#endif	// #ifdef N_Ef
+				// N_Ef(vxrwh81, "Illegal uuid=@STR k=@INT c=@CHAR", urn_uuid_str, k, c);
 					rv = -1;
 				}
 				c = *urn_uuid_ptr++;
@@ -56,9 +45,7 @@ int nvmeibt_urn_uuid_str_to_union_uuid(union nvmeib_uuid *uuid, const char *urn_
 			else if (c >= 'A' || c <= 'F')
 				v = (v << 4) | (c - 'A' + 10);
 			else {
-#ifdef N_Ef
-				N_Ef(vxrwh82, "Illegal uuid=@STR k=@INT c=@CHAR", urn_uuid_str, k, c);
-#endif	// #ifdef N_Ef
+				// N_Ef(vxrwh82, "Illegal uuid=@STR k=@INT c=@CHAR", urn_uuid_str, k, c);
 				rv = -1;
 			}
 		}
@@ -78,28 +65,24 @@ int nvmeibt_urn_uuid_str_to_union_uuid(union nvmeib_uuid *uuid, const char *urn_
 	return rv;
 }
 
-int nvmeibt_urn_uuid_to_union_uuid(union nvmeib_uuid *uuid, const struct nvmeibt_urn_uuid *urn_uuid)
-{
+int nvmeibt_urn_uuid_to_union_uuid(union nvmeib_uuid *uuid, const struct nvmeibt_urn_uuid *urn_uuid) {
 		return nvmeibt_urn_uuid_str_to_union_uuid(uuid, urn_uuid->str);
 }
 
 #define HEX_NIBBLE(v) ((v)<=9 ? (v)+'0' : (v)-10+'a')
-static uint32_t rotr(uint32_t x, unsigned int n)
-{
+static uint32_t rotr(uint32_t x, unsigned int n) {
 	// optimized to a single "ror" instruction
-	const unsigned int  mask = (8*sizeof(x) - 1);
+	const unsigned int mask = (8*sizeof(x) - 1);
 	n &= mask;
 	return (x>>n) | (x<<( (-n)&mask ));
 }
 
-void nvmeibt_union_uuid_to_urn_uuid_in_place(const union nvmeib_uuid *uuid, char *str)
-{
+void nvmeibt_union_uuid_to_urn_uuid_in_place(const union nvmeib_uuid *uuid, char *str) {
 	union _uuid_swapper u = *((union _uuid_swapper *)uuid);
 	int i;
 	unsigned int j;
 	char *p = str;
 	unsigned short v;
-
 #if __BYTE_ORDER__ == __ORDER_LITTLE_ENDIAN__
 	u.lw[0] = __builtin_bswap32(u.lw[0]);
 	u.sw[2] = __builtin_bswap16(u.sw[2]);
@@ -118,25 +101,20 @@ void nvmeibt_union_uuid_to_urn_uuid_in_place(const union nvmeib_uuid *uuid, char
 	*p=0;
 }
 
-struct nvmeibt_urn_uuid nvmeibt_union_uuid_to_urn_uuid(const union nvmeib_uuid *uuid)
-{
+struct nvmeibt_urn_uuid nvmeibt_union_uuid_to_urn_uuid(const union nvmeib_uuid *uuid) {
 	struct nvmeibt_urn_uuid result;
-
 	if ((uuid->ll[0] == nvmeibt_dummy_uuid.ll[0]) && (uuid->ll[1] == nvmeibt_dummy_uuid.ll[1])) {
 		result.str[0] = 0;
-	}
-	else {
+	} else {
 		nvmeibt_union_uuid_to_urn_uuid_in_place(uuid, result.str);
 	}
 	return result;
 }
 
-void generate_random_uuid(union nvmeib_uuid *uuid)
-{
+void generate_random_uuid(union nvmeib_uuid *uuid) {
 	struct timespec ts;
-	static unsigned int		noisy_tv_nsec = 0;
+	static unsigned int noisy_tv_nsec = 0;
 	int				rv;
-
 #ifdef FOUND_SYS_RANDOM_H
 	rv = getrandom(uuid, sizeof(*uuid), GRND_RANDOM | GRND_NONBLOCK);
 #else
@@ -144,9 +122,7 @@ void generate_random_uuid(union nvmeib_uuid *uuid)
 	errno = ENOPKG;
 #endif
 	if (rv) {
-#ifdef N_Tf
-		N_Tf(9sjcb4t, "getrandom() returned @AUTO_ERRNO. Using a less 'random' method");
-#endif	// #ifdef N_Tf
+		// N_Tf(9sjcb4t, "getrandom() returned @AUTO_ERRNO. Using a less 'random' method");
 		// Add noise to random
 		getnstimeofday(&ts);
 		if (!noisy_tv_nsec) {
