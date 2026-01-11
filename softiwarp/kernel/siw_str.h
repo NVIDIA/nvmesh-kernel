00d8dba23f5060017e1364e665a7886ac49c0bff
Guy Eisenberg <geisenberg@nvidia.com>
2026-01-11 17:12:29 +0200

    [NVMESH-7363] - fix siw_strlcpy accesses out-of-bound array element
    Change-Id: I7ebe6ce62fd5febbb4b9a5872bffb05680e90763
    Signed-off-by: Guy Eisenberg <geisenberg@nvidia.com>

--- softiwarp/kernel/siw_str.h
+++ softiwarp/kernel/siw_str.h
@@ -15,7 +15,14 @@ static inline size_t siw_strlcpy(char *dst, const char *src, size_t max_len_unsi
 {
 	int max_len = (int)max_len_unsigned;
 
-	size_t src_len = src ? strlen(src) : 0;
+	size_t src_len;
+
+	if (!dst || !src) {
+		return 0; 
+	}
+
+	src_len = strlen(src);
+	
 	if (max_len < 0) {
 		// Do nothing
 	} else if ((int)src_len + 1 <= max_len) {
