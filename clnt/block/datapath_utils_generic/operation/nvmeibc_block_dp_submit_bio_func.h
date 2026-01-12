acbba63fb210ab6637c9f7276ff646867697b33a
Roman Yakovenko <ryakovenko@nvidia.com>
2026-01-12 19:28:19 +0200

    [NVMESH-7412] disable io pet on multi blockset operations
    Change-Id: If30c1f302ffe7c3681a8816551b828a9db857fb3
    Signed-off-by: Roman Yakovenko <ryakovenko@nvidia.com>

--- clnt/block/datapath_utils_generic/operation/nvmeibc_block_dp_submit_bio_func.h
+++ clnt/block/datapath_utils_generic/operation/nvmeibc_block_dp_submit_bio_func.h
@@ -7,6 +7,10 @@
 #include "block/datapath_utils_generic/dp_io_stats/nvmeibc_b_dp_iostats.h"
 #include "nvmeibc_block.h"
 
+#ifndef __KERNEL__
+	#include "nvmeib_math.h"	/* For round_up/round_down in user-space */
+#endif
+
 /**************************** H interface **********************************/
 int execute_bio(struct bio *bio, ulong now);
 
@@ -50,6 +54,51 @@ static inline u32 __get_split(const struct nvmeibc_block_device *nd, const enum 
 	return nd->dp.alignment_sectors.read;
 }
 
+/**
+ * __is_multiple_blocksets - Check if an I/O spans multiple blocksets
+ * @lba_bio_start_s: Starting LBA in kernel sectors (512 bytes)
+ * @size: Size in bytes
+ * @slice_size: Number of blocks (4KB) per slice (e.g., 128 for EC 4+2, 256 for EC 8+2)
+ *
+ * A blockset in VLBA (volume) space is LOCKSET_SLICES (32 blocks = 128KB on single disk)
+ * times the slice_size (number of data disks). For example:
+ *  - EC 4+2: blockset = 32 * 128 = 4096 blocks = 512KB
+ *  - EC 8+2: blockset = 32 * 256 = 8192 blocks = 1MB
+ *  - Mirror/JBOD: blockset = 32 * 1 = 32 blocks = 128KB (BYTES_IN_LOCKSET)
+ *
+ * Note: BYTES_IN_LOCKSET (128KB) represents the lock granularity on a single disk segment.
+ * The actual blockset size in volume space depends on the RAID configuration.
+ *
+ * Returns: true if I/O spans multiple blocksets, false otherwise
+ */
+static inline bool __is_multiple_blocksets(ulong lba_bio_start_s, long size, u32 slice_size)
+{
+	ulong bio_start_b;
+	ulong blockset_size_b;
+	ulong align_down_start;
+	ulong align_up_end;
+
+	BUG_ON(size <= 0);
+	BUG_ON(slice_size == 0);
+
+	/* Convert start from sectors to bytes */
+	bio_start_b = lba_bio_start_s << KERNEL_SECTOR_SHIFT;
+
+	/* Calculate blockset size in bytes for VLBA space:
+	 * blockset = LOCKSET_SLICES (32 blocks) * slice_size * block_size (4KB)
+	 * LOCKSET_SLICES is already in blocks, so multiply by slice_size and block size */
+	blockset_size_b = ((ulong)LOCKSET_SLICES * slice_size) << NVMEIBC_SECTOR_SHIFT;
+
+	/* Align down the start address to blockset boundary */
+	align_down_start = round_down(bio_start_b, blockset_size_b);
+
+	/* Align up the end address to blockset boundary */
+	align_up_end = round_up(bio_start_b + size, blockset_size_b);
+
+	/* If the span is larger than one blockset, return true */
+	return (align_up_end - align_down_start) > blockset_size_b;
+}
+
 #define BIO_LEADER_REF  0x40000000
 int execute_bio(struct bio *bio, unsigned long now)
 {
@@ -66,6 +115,7 @@ int execute_bio(struct bio *bio, unsigned long now)
 	long total_size_b = __GET_BI_SIZE(bio);
 	long bio_part_ofst_s = 0UL, bio_part_size_b;
 	struct nvmeib_cpu_mask_info cpu_mask_info;
+	struct nvmeib_pet_base_controller* io_pet_controller;
 
 	/* Subset of out of bound tests taken from operation code. Needed to avoid wrong split */
 	rv = block_api_os_verify_bio_geometry(bio);
@@ -86,7 +136,8 @@ int execute_bio(struct bio *bio, unsigned long now)
 			if (bio_part_size_b > max_bytes)
 				bio_part_size_b = max_bytes;
 		}
-		o = nvmeibc_operation_create_with_biopart(nd->dp.sizeof_operation, nd->io_pet_controller);
+		io_pet_controller = __is_multiple_blocksets(cur_start_s, bio_part_size_b, nd->dp.p.slice_size) ? NULL : nd->io_pet_controller;
+		o = nvmeibc_operation_create_with_biopart(nd->dp.sizeof_operation, io_pet_controller);
 		if (unlikely(!o)) {
 			rv = -ENOMEM;
 			goto _out;
