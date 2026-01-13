be2ca92c242a2de7014ae69f9aaccafa31c56c51
danielhsh <DanielHe@nvidia.com>
2026-01-13 12:36:48 +0200

    NVMESH-7489: toma_rpc, simplify, add version
    Change-Id: Ie886d01dc91004fe8051de01b83af12b2cf00613
    Signed-off-by: danielhsh <DanielHe@nvidia.com>

--- tools/toma_rpc/toma_rpc.c
+++ tools/toma_rpc/toma_rpc.c
@@ -1,92 +1,83 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <sys/select.h>
 #include <sys/socket.h>
 #include <sys/un.h>
 #include <unistd.h>
 #include <sys/ioctl.h>
 #include <sys/types.h>
-#include <sys/stat.h>
 #include <string.h>
 #include <errno.h>
 
-
-#define TOMA_RPC_PATH "/var/log/nvmesh/toma_rpc"
+#define UTIL_VERSION  "version=1.0"
 #define MSG_SIZE 256
 
 int main(int argc, char *argv[])
 {
 	struct sockaddr_un sun;
-	int fd;
-	int i, len, rc;
+	int fd, i, len, total_bytes_received = 0;
 	char inbuf[MSG_SIZE+1];
 
 	if (getuid()) {
-#if 0
-		char **new_argv = (char **)malloc(sizeof(char*)*(argc+2));
-		memcpy(new_argv+1, argv, argc*sizeof(char *));
-		new_argv[argc+1] = NULL;
-		new_argv[0] = "sudo";
-		printf("(You're not root, attempting automatic sudo)\n");
-		execvp(new_argv[0], new_argv);
-		return 0;		//never reached
-#endif
-		printf("Permission denied\n");
+		printf(UTIL_VERSION ", Permission denied\n");
 		return -1;
 	}
-
 	if (argc<2) {
-		printf("No command specified. Try 'help'.\n");
+		printf(UTIL_VERSION ", No command specified. Try 'help'.\n");
 		return -1;
 	}
 
 	inbuf[0] = 0;
 	len = 0;
 	for (i=1; i<argc; i++) {
-		rc = snprintf(inbuf+len, MSG_SIZE - len - 1, "%s ", argv[i]);
-		if (rc<0)
+		const int available = MSG_SIZE - len - 2;
+		const int n_bytes = snprintf(inbuf+len, available, "%s ", argv[i]);
+		if (n_bytes < 0)
 			return -1;
-		len += rc;
+		if (n_bytes >= available) {	// Check if output was truncated
+			printf("Command too long (truncated)\n");
+			return -1;
+		}
+		len += n_bytes;
 	}
-	if (len>0)
-		inbuf[len-1] = 0;
-	strcat(inbuf, "\n");
+	if (len <= 0) {
+		printf("Wrong command specified, len=%d. Try 'help'.\n", len);
+		return -1;
+	}
+	inbuf[len-1] = '\n';
+	inbuf[len] = 0;
 
 	fd = socket(PF_UNIX, SOCK_STREAM, 0);
-	if (fd<0) {
+	if (fd < 0) {
 		perror("socket");
 		return -1;
 	}
-
 	sun.sun_family = PF_UNIX;
-	strncpy(sun.sun_path, TOMA_RPC_PATH, sizeof(sun.sun_path)-1);
-	rc = connect(fd, (struct sockaddr *)&sun, sizeof(sun));
-	if (rc<0) {
+	strncpy(sun.sun_path, "/var/log/nvmesh/toma_rpc", sizeof(sun.sun_path)-1);
+	sun.sun_path[sizeof(sun.sun_path)-1] = '\0';  // Ensure null termination
+	if (connect(fd, (struct sockaddr *)&sun, sizeof(sun)) < 0) {
 		perror("connect");
 		close(fd);
 		return -1;
 	}
+	if (send(fd, inbuf, strlen(inbuf), 0) < 0) {
+		perror("send");
+		close(fd);
+		return -1;
+	}
 
-	send(fd, inbuf, strlen(inbuf), 0);
-	while (1) {
-		rc = recv(fd, inbuf, sizeof(inbuf)-1, 0);
-		if (rc == 0) {
-			// graceful
-			printf("\n[end]\n");
+	while (1) {		// Receive the reply, if reply is larger then this loop will do multiple iterations
+		const int n_bytes = recv(fd, inbuf, sizeof(inbuf)-1, 0);
+		if (n_bytes == 0) {
+			printf("\n[end %db]\n", total_bytes_received);
+			break;
+		} else if (n_bytes < 0) {
+			printf("\n[error %m]\n");
 			break;
 		}
-		else if (rc<0) {
-			if (errno != EAGAIN && errno != EINTR) {
-				printf("\n[error %m]\n");
-				break;
-			}
-			continue;
-		}
-
-		inbuf[rc] = 0;
+		inbuf[n_bytes] = 0;
 		printf("%s", inbuf);
+		total_bytes_received += n_bytes;
 	}
 	close(fd);
-
 	return 0;
 }
