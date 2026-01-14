f06431fd540ecc6572bde9dc973e868afd5e54da
Alexander Rokhlin <alexander@nvidia.com>
2026-01-14 10:56:10 +0200

    NVMESH-7488: Send TOMA_NOT_READY when a REGISTER msg received during registrant unregister proccess
     - Before the commit toma sent REGISTER_ACK
       with old reg_ctx params(conversation id and lock_id)
    
    Change-Id: I09798a81bf79bf5f14636c9e10fcacd786610e61
    Signed-off-by: Alexander <alexander@nvidia.com>

--- toma/nvmeibt_register.c
+++ toma/nvmeibt_register.c
@@ -2554,6 +2554,10 @@ static BOOL is_valid_register_req(struct nvmeibt_registrant_ctx *incoming_reg_ct
 		goto toma_not_ready;
 	}
 
+	if (existing_reg_ctx_same_lockid && existing_reg_ctx_same_lockid->is_processing_registrant_removal) {
+		refusal_reason = NVMEIBT_CLIENT_TR_REASON_UNREGISTER_IN_PROGRESS;
+		goto toma_not_ready;
+	}
 	if (!existing_reg_ctx_same_lockid && lock_id_cache_is_lockid_taken(seg_active, incoming_reg_ctx->reg_lock_id)) {
 		// No active registrant, but its journal entries were not fully cleaned yet
 		refusal_reason = NVMEIBT_CLIENT_TR_REASON_LOCKID_ALREADY_TAKEN;
