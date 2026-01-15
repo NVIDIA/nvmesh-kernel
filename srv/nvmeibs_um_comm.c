bdd54b008a08eb61a7d7e9237f4e082f51b47f6f
danielhsh <DanielHe@nvidia.com>
2026-01-15 10:28:33 +0200

    Server: prevent crash on wrong toma message
    Change-Id: I9cfb79c2a680ad9595bd55223a6a4913575fbade
    Signed-off-by: danielhsh <DanielHe@nvidia.com>

--- srv/nvmeibs_um_comm.c
+++ srv/nvmeibs_um_comm.c
@@ -2652,8 +2652,8 @@ static struct nvmeib_nl_uk_comm_rep * get_rep(struct nvmeib_nl_uk_comm_msg *msg)
 		break;
 #endif
 	case csc_remove_disk_ack: default:	// All those codes do not require response from the server
-		WARN(true, "Going to crash: wrong msg->opcode=%d", msg->opcode);
-		BUG();
+		WARN(true, "nvmeibs BUG: Wrong toma message in_msg->opcode[%d] not expectig a reply, bailing out with wrong reply instead of crash!", msg->opcode);
+		rep = &((struct nvmeib_zero_disk_reply *)msg->data)->base;
 		break;
 	};
 	NFOUT;
