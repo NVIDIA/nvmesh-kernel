b82a76b4c5b450f945a69b060236da78b2a990fd
Roman Yakovenko <ryakovenko@nvidia.com>
2026-01-21 14:17:33 +0200

    [NVMESH-7415] - fix review comments
    Change-Id: Ibc62b98e8a645da411bb02961f3e135fab21274f
    Signed-off-by: Roman Yakovenko <ryakovenko@nvidia.com>

--- clnt/block/datapath_utils_generic/nvmeibc_block_dp_io_generic_cmds.c
+++ clnt/block/datapath_utils_generic/nvmeibc_block_dp_io_generic_cmds.c
@@ -303,13 +303,13 @@ static void __nvmeibc_cmd_data_and_metadata_pet_describe(struct nvmeibc_block_co
 			*/
 			if (nvmeibc_raid_is_ec(r)) {
 				NVMEIBC_IO_PET_MSG_NORM(journal,
-							"    content(idx=%hhu, first_8b = 0x%llx, md = 0x%llx<union nvmeibc_block_dp_ec_data_block_md>)", 
-							numeric_downcast(u8, i), *data_first_content, md->raw);
+							"    content(idx=%hhu, first_8b = 0x%llx, is_parity = %hhu, md = 0x%llx<union nvmeibc_block_dp_ec_data_block_md>)", 
+							numeric_downcast(u8, i), *data_first_content, (u8)bcmd->is_parity, md->raw);
 			} else if (nvmeibc_raid_is_mirror(r)) {
 				//mirror datapath is configurable and may use or not use edic, but since we are playing with the backdoor, it is better to print it anyway
 				NVMEIBC_IO_PET_MSG_NORM(journal,
 							"    content(idx=%hhu, first_8b = 0x%llx, edic = 0x%x)",
-							numeric_downcast(u8, i), *data_first_content, nvmeibc_block_dp_ec_md_get_edic(md, true));
+							numeric_downcast(u8, i), *data_first_content, nvmeibc_block_dp_ec_md_get_edic(md, bcmd->is_parity));
 			}
 			md = (union nvmeibc_block_dp_ec_data_block_md *)((u8*)md + md_size);
 		} else if (data_first_content != NULL) {
@@ -327,7 +327,7 @@ static void __nvmeibc_cmd_execute_pet_describe(struct nvmeibc_block_command *cmd
 	enum nvmeib_block_io_op op = cmds->o->op;
 	u32 nlbas;
 	struct nvmeibc_d_rdma_comp* dc;
-	// For TRIM, use bcmd->nlbas directly; for R/W, calculate from buffer length.
+	
 	if (op == NVMEIB_BLOCK_IO_OP_DISCARD) {
 		nlbas = nvmeib_get_ndb_discard_range(bcmd, NVMEIB_DSM_RANGE_ENCODING_NATIVE).nlb;    // get_dsm.
 	} else {
@@ -339,7 +339,7 @@ static void __nvmeibc_cmd_execute_pet_describe(struct nvmeibc_block_command *cmd
 		"dp_cmds_execute_cmd(sgmnt=%hhu, dlba=0x%llx, nlbas=%u, raid_cur_stage=%hhu<enum e_cmds_stage>)",
 		sgmnt_idx, __cmd_start(*bcmd), nlbas, (u8)cmds->raid_cur_stage);
 	
-	__nvmeibc_cmd_data_and_metadata_pet_describe(bcmd, false/*is_completion*/);
+	__nvmeibc_cmd_data_and_metadata_pet_describe(bcmd, /*is_completion*/false);
 
 	if (dp_cmds_pigbck_has_any(cmd)) {
 		dc = dp_cmds_get_pigbck_comp_dc(cmd);
@@ -765,7 +765,7 @@ static inline void __nvmeibc_cmd_completion_pet_describe(struct operation *o, st
 						   cmd->o_rv ? NVMEIB_PET_SEVERITY_WARNING : NVMEIB_PET_SEVERITY_NORMAL,  
 						   numeric_downcast(u8, __dp_get_sgmnt_idx_from_ds(cmd->ds)), cmd->o_rv, cmd->iocmd->comp.comp_code);
 		
-		__nvmeibc_cmd_data_and_metadata_pet_describe(rldr, true/*is_completion*/);
+		__nvmeibc_cmd_data_and_metadata_pet_describe(rldr, /*is_completion*/true);
 
 		if (dp_cmds_pigbck_has_any(rldr->iocmd)) {
 			dc = dp_cmds_get_pigbck_comp_dc(rldr->iocmd);
