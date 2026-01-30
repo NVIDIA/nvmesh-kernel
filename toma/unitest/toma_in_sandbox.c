a11a356a1a50a12a82824dacb2610ea4262ea9e2
Colin Bennett <cbennett@nvidia.com>
2026-01-30 15:01:43 -0800

    NVMESH-7438: increase SANDBOX_TERMINATE_AFTER_N_LOOPS to 120
    We need to run Toma longer to get the periodic messages like
    reportTarget to be sent.
    
    Currently the sandbox epoll_wait() function sleeps for 100 ms, so the
    Toma sandbox test will take approximately N_LOOPS / 10 seconds until it
    shuts down Toma (and that takes another few seconds to complete during
    the shutdown process).
    
    The reportTarget message in particular is only sent once per second
    maximum -- we might want to do some accelerated time simulation to speed
    up test runs in the future as we expand the test suite.
    
    Change-Id: I58a3f62d6e1a528c0d590c56ae45b0ee75e292a6
    Signed-off-by: Colin Bennett <cbennett@nvidia.com>

--- toma/unitest/toma_in_sandbox.c
+++ toma/unitest/toma_in_sandbox.c
@@ -1312,7 +1312,7 @@ int epoll_ctl(int efd, enum EPOLL_CTL op, int __fd, struct epoll_event *ev) {
 }
 
 int epoll_wait(int efd, struct epoll_event *evs, int man_events, int __timeout) {
-	#define SANDBOX_TERMINATE_AFTER_N_LOOPS 50
+	#define SANDBOX_TERMINATE_AFTER_N_LOOPS 120
 	struct globa_epoll *ep = &sys->TSB_epoll;
 	static uint64_t loop_idx = 0;
 	static bool is_shutting_down = false;
