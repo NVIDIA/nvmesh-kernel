bc81b2f6b39db722ed774116916f3ba8c516eb50
Wentao Cai <wcai@nvidia.com>
2026-01-30 18:04:21 -0600

    Toma gpt util: Add gpt_util.sh script that auto finds toma binary and executes gptutil
    This is to replace the legacy toma_link approach of searching toma
    
    Change-Id: Icc4db36d4ae3268180d40aa1ab9a79fd8f6091b0
    Signed-off-by: Wentao Cai <wcai@nvidia.com>

---
+++ toma/scripts/gpt_util.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+toma_executable=`ls bin/release/nvmeibt_toma`
+kernel_release=`uname --kernel-release`;
+toma_executable_public=`ls /opt/nvmesh/target-repo/target_*${kernel_release}/toma/bin/release/nvmeibt_toma`;
+if [ "_${toma_executable_public}" != "_" ]; then
+	if [ "_${toma_executable}" == "_" ]; then
+		toma_executable=${toma_executable_public};
+	else
+		echo "	BTW, found file:       ${toma_executable_public}";
+	fi
+fi
+read -p "TOMA executable (default=${toma_executable})? " file_name
+if [ "_${file_name}" != "_" ]; then
+	toma_executable=${file_name};
+fi;
+
+cmdline="sudo ${toma_executable} gpt_util $@"
+echo Running ${cmdline}
+${cmdline}
+
