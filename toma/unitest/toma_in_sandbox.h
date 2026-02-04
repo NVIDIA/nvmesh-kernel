c65de17331f05aefd7e9c63aa2eee26214bf495f
danielhsh <DanielHe@nvidia.com>
2026-02-04 17:41:18 +0200

    Toma: print lib rdkafka version to kafka.txt
    Change-Id: Ia9f2e4f9bb1d29d0606a926270c379f06a413356
    Signed-off-by: danielhsh <DanielHe@nvidia.com>

--- toma/unitest/toma_in_sandbox.h
+++ toma/unitest/toma_in_sandbox.h
@@ -306,6 +306,8 @@ enum my_rd_kafka_producer_flags { RD_KAFKA_MSG_F_FREE = 0x1, RD_KAFKA_MSG_F_COPY
 int rd_kafka_produce(rd_kafka_topic_t *kt, int32_t partition, int msgflags, void *payload, size_t len, const void *key, size_t keylen, void *msg_opaque);
 rd_kafka_resp_err_t rd_kafka_fatal_error(rd_kafka_t *rk, char *errstr, size_t errstr_size);
 #define LOG_DEBUG (5)
+static inline int         rd_kafka_version(    void)	{ return 0x20102; }
+static inline const char* rd_kafka_version_str(void)	{ return "0x20102"; }
 
 /************************************* udev ************************************/
 #include "interfaces/nvme/nvmeibt_udev.h"
