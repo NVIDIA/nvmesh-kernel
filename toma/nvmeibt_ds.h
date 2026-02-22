0dffdc503eda6d067becb1a97356b2f022c38436
danielhsh <DanielHe@nvidia.com>
2026-02-22 20:39:00 +0200

    Toma: remove ds deprecated code
    Change-Id: If0671bfe4d605731f38a35bb6d11ca7532e9a6dc
    Signed-off-by: danielhsh <DanielHe@nvidia.com>

--- toma/nvmeibt_ds.h
+++ toma/nvmeibt_ds.h
@@ -30,10 +30,6 @@ struct xdlist {
 	struct xdlist *next;
 	struct xdlist *prev;
 	struct xdlist_with_size *parent;
-#ifdef TOMA_DEBUG
-	void *add_bt_buff[8];
-	int add_bt_nptrs;
-#endif
 };
 
 /* double linked list + size*/
@@ -55,30 +51,15 @@ struct xdlist_with_size {
 		} *data; \
 	}
 
-#ifndef TOMA_DEBUG
-#	define XDLIST_INIT(name) {{ {&((name).list_with_size.list), &((name).list_with_size.list), &((name).list_with_size)}, 0}}
+#define XDLIST_INIT(name) {{ {&((name).list_with_size.list), &((name).list_with_size.list), &((name).list_with_size)}, 0}}
 
-#	define XDLIST_INIT_LINK(field, parent_list) \
-		do { \
-			(field)->next = (field); \
-			(field)->prev = (field); \
-			(field)->parent = (parent_list); \
-		} while (0)
-#else
-#	define XDLIST_INIT(name) {{ {&((name).list_with_size.list), &((name).list_with_size.list), &((name).list_with_size), {0}, 0}, 0}}
+#define XDLIST_INIT_LINK(field, parent_list) ({ \
+	(field)->next = (field); \
+	(field)->prev = (field); \
+	(field)->parent = (parent_list); \
+})
 
-#	define XDLIST_INIT_LINK(field, parent_list)								\
-		do {																\
-			(field)->next = (field);										\
-			(field)->prev = (field);										\
-			(field)->parent = (parent_list);								\
-			memset((field)->add_bt_buff, 0, sizeof((field)->add_bt_buff));	\
-			(field)->add_bt_nptrs = 0;										\
-		} while (0)
-#endif
-
-#define XDLIST_NULL(field) \
-	((field)->next == (field) && (field)->prev == (field))
+#define XDLIST_NULL(field) 	((field)->next == (field) && (field)->prev == (field))
 
 #define XDLIST_HEAD_INIT(name) do {												\
 	XDLIST_INIT_LINK(&(name)->list_with_size.list, &((name)->list_with_size));	\
@@ -95,33 +76,26 @@ struct xdlist_with_size {
 	((struct xdlist *) ((char *)(elem) + XDLIST_OFFSET(head)))
 
 #ifdef __cplusplus
-    #define XDLIST_CHECK_TYPES(     head , elem ) false
-    #define XDLIST_CHECK_HEAD_TYPES(head1, head2) false
+	#define XDLIST_CHECK_TYPES(     head , elem ) false
+	#define XDLIST_CHECK_HEAD_TYPES(head1, head2) false
 #else
-#define XDLIST_CHECK_TYPES(head, elem) \
-	BUILD_BUG_IF_ZERO(__builtin_types_compatible_p(XDLIST_TYPE(head), \
-	typeof(elem)))
-
-#define XDLIST_CHECK_HEAD_TYPES(head1, head2) \
-	BUILD_BUG_IF_ZERO(__builtin_types_compatible_p(XDLIST_TYPE(head1), \
-	XDLIST_TYPE(head2)))
+	#define XDLIST_CHECK_TYPES(     head, elem)   BUILD_BUG_IF_ZERO(__builtin_types_compatible_p(XDLIST_TYPE(head), typeof(elem)))
+	#define XDLIST_CHECK_HEAD_TYPES(head1, head2) BUILD_BUG_IF_ZERO(__builtin_types_compatible_p(XDLIST_TYPE(head1), XDLIST_TYPE(head2)))
 #endif
 
 #define XDLIST_EMPTY(head) ((head)->list_with_size.list.next == &(head)->list_with_size.list)
 
 /* unsafe - please try not to use */
 #define XDLIST_FIRST_(head) XDLIST_ELEM((head), (head)->list_with_size.list.next)
-#define XDLIST_LAST_(head) XDLIST_ELEM((head), (head)->list_with_size.list.prev)
+#define XDLIST_LAST_(head)  XDLIST_ELEM((head), (head)->list_with_size.list.prev)
 #define XDLIST_NEXT_(elem, field) \
 	container_of((elem)->field.next, typeof(*(elem)), field)
 #define XDLIST_PREV_(elem, field) \
 	container_of((elem)->field.prev, typeof(*(elem)), field)
 
 /* the safe version */
-#define XDLIST_FIRST(head)\
-    (XDLIST_EMPTY(head) ? NULL : XDLIST_FIRST_(head))
-#define XDLIST_LAST(head)\
-    (XDLIST_EMPTY(head) ? NULL : XDLIST_LAST_(head))
+#define XDLIST_FIRST(head)	(XDLIST_EMPTY(head) ? NULL : XDLIST_FIRST_(head))
+#define XDLIST_LAST(head)	(XDLIST_EMPTY(head) ? NULL : XDLIST_LAST_(head))
 
 #define XDLIST_SAFE(head, list_with_size) \
     ((list_with_size.list) == &(head)->list_with_size.list) ? NULL : XDLIST_ELEM((head), (list_with_size.list))
@@ -134,44 +108,6 @@ struct xdlist_with_size {
 		XDLIST_SAFE((head), XDLIST_FIELD((head), (elem))->prev))
 
 /* a general add */
-#ifdef TOMA_DEBUG
-#	define XDLIST_ADD(head, _prev, _next, elem) \
-		do { \
-			if (XDLIST_CHECK_TYPES(head, elem)); \
-			else for (struct xdlist *CONCAT(new_link_, __LINE__), \
-					*CONCAT(prev_, __LINE__) = _prev, \
-					*CONCAT(next_, __LINE__) = _next; ; ) { \
-				CONCAT(new_link_, __LINE__) 		= XDLIST_FIELD((head), (elem)); \
-				CONCAT(next_, __LINE__)->prev		= CONCAT(new_link_, __LINE__); \
-				CONCAT(new_link_, __LINE__)->next 	= CONCAT(next_, __LINE__); \
-				CONCAT(new_link_, __LINE__)->prev 	= CONCAT(prev_, __LINE__); \
-				CONCAT(prev_, __LINE__)->next 		= CONCAT(new_link_, __LINE__); \
-				CONCAT(new_link_, __LINE__)->parent	= &((head)->list_with_size); \
-				CONCAT(new_link_, __LINE__)->add_bt_nptrs = backtrace(CONCAT(new_link_, __LINE__)->add_bt_buff, 8);	\
-				((head)->list_with_size.size)++; \
-				break; \
-			} \
-		} while (0)
-#	define NXDLIST_ADD_CHECK(name, head, _prev, _next, elem)							\
-		do {												\
-			struct xdlist *CONCAT(l_, __LINE__) = XDLIST_FIELD((head), (elem));			\
-			if (!XDLIST_NULL(CONCAT(l_, __LINE__))) {						\
-				int j, add_bt_nptrs;								\
-				void *add_bt_buff[8];								\
-				char **strings;									\
-				add_bt_nptrs = backtrace(add_bt_buff, 8);					\
-				strings = backtrace_symbols(add_bt_buff, add_bt_nptrs);				\
-				N_Ef(name ## _error_1, "XDLIST_ADD_CHECK, expected XDLIST_NULL()");				\
-				for (j = 0; j < add_bt_nptrs; j++)						\
-					N_Ef(name ## _error_2, " @STR", strrchr(strings[j], '/'));					\
-				strings = backtrace_symbols((CONCAT(l_, __LINE__))->add_bt_buff, (CONCAT(l_, __LINE__))->add_bt_nptrs); 	\
-				N_Ef(name ## _error_3, "Previous Add BT");							\
-				for (j = 0; j < (CONCAT(l_, __LINE__))->add_bt_nptrs; j++)					\
-					N_Ef(name ## _error_4, " @STR", strrchr(strings[j], '/'));					\
-			} else											\
-				XDLIST_ADD(head, _prev, _next, elem);						\
-		} while (0)
-#else
 #	define XDLIST_ADD(head, _prev, _next, elem) \
 			do { \
 				if (XDLIST_CHECK_TYPES(head, elem)); \
@@ -195,14 +131,12 @@ struct xdlist_with_size {
 			else 											\
 				XDLIST_ADD(head, _prev, _next, elem);						\
 		} while (0)
-#endif
 
 /* add to head or tail */
 #define XDLIST_ADD_HEAD(head, elem) XDLIST_ADD(head, &(head)->list_with_size.list, (head)->list_with_size.list.next, elem)
 #define XDLIST_ADD_TAIL(head, elem) XDLIST_ADD(head, (head)->list_with_size.list.prev, &(head)->list_with_size.list, elem)
 #define XDLIST_ADD_HEAD_CHECK(head, elem) XDLIST_ADD_CHECK(head, &(head)->list_with_size.list, (head)->list_with_size.list.next, elem)
 #define NXDLIST_ADD_TAIL_CHECK(name, head, elem) NXDLIST_ADD_CHECK(name, head, (head)->list_with_size.list.prev, &(head)->list_with_size.list, elem)
-#if 1
 #define _XDLIST_SPLICE(head, _prev, _next, to_head) \
 	do { \
 		if (XDLIST_CHECK_HEAD_TYPES(head, to_head) || XDLIST_EMPTY(head)); \
@@ -221,10 +155,9 @@ struct xdlist_with_size {
 				break; \
 		} \
 	} while (0)
-#endif
+
 /* in splice head is the source and elem is the destination */
 #define XDLIST_SPLICE(from_head, to_head) _XDLIST_SPLICE(from_head, &(to_head)->list_with_size.list,  (to_head)->list_with_size.list.next, to_head)
-#define XDLIST_SPLICE_TAIL(from_head, to_head) //_XDLIST_SPLICE(from_head, (to_head)->list_with_size.list.prev,  &(to_head)->list_with_size.list, to_head)
 
 /* delete an element by the link */
 #define XDLIST_DEL(field) do {							\
@@ -241,50 +174,12 @@ struct xdlist_with_size {
 	if (XDLIST_CHECK_TYPES(head, elem)); \
 	else XDLIST_DEL(XDLIST_FIELD(head, elem))
 
-/* number of elements*/
-#define XDLIST_N_ELEMNTS(head) \
-	(head)->list_with_size.size
+#define XDLIST_N_ELEMNTS(head)	(head)->list_with_size.size
 
 /* iteration */
-#ifndef TOMA_DEBUG
-#	define XDLIST_LOOP_NEXT(var, head) \
-		XDLIST_ELEM((head), XDLIST_FIELD((head), (var))->next)
-#	define XDLIST_LOOP_PREV(var, head) \
-		XDLIST_ELEM((head), XDLIST_FIELD((head), (var))->prev)
-#else
-	/* For the debug versions, check for endless loops (var->link.next = &var->link) */
-	static inline struct xdlist *xdlist_loop_next_check(const struct xdlist *head, struct xdlist *xd_link)
-	{
-		if (xd_link->next == xd_link) {
-			if (xd_link == head) {
-				// _Tf("Looping over empty list (head %p)\n", head);
-			} else {
-				N_Ef(t_04_toma_ds, "Endless Loop Detected! link->next (@PTR) = link", xd_link->next);
-				nvmeibt_abort(ES_FATAL);
-			}
-		}
-		return xd_link->next;
-	}
+#define XDLIST_LOOP_NEXT(var, head) XDLIST_ELEM((head), XDLIST_FIELD((head), (var))->next)
+#define XDLIST_LOOP_PREV(var, head) XDLIST_ELEM((head), XDLIST_FIELD((head), (var))->prev)
 
-	static inline struct xdlist *xdlist_loop_prev_check(const struct xdlist *head, struct xdlist *xd_link)
-	{
-		if (xd_link->prev == xd_link) {
-			if (xd_link == head) {
-				// _Tf("Looping over empty list (head %p)\n", head);
-			} else {
-				N_Ef(t_05_toma_ds, "Endless Loop Detected! link->prev (@PTR) = link", xd_link->prev);
-				nvmeibt_abort(ES_FATAL);
-			}
-		}
-		return xd_link->prev;
-	}
-
-#	define XDLIST_LOOP_NEXT(var, head) \
-		XDLIST_ELEM((head), (xdlist_loop_next_check(&(head)->list_with_size.list, XDLIST_FIELD((head), (var)))))
-
-#	define XDLIST_LOOP_PREV(var, head) \
-		XDLIST_ELEM((head), (xdlist_loop_prev_check(&(head)->list_with_size.list, XDLIST_FIELD((head), (var)))))
-#endif
 /* loop with modifing the list */
 #define XDLIST_FOREACH(var, head) \
 	for ((var) = XDLIST_ELEM((head), (head)->list_with_size.list.next); \
@@ -462,29 +357,11 @@ static inline uint32_t xhash_32(uint32_t val, uint32_t bits)
 
 
 /* hashtable loop */
-#define XHASHTABLE_FOR_EACH(var, name) \
-	_Pragma ("GCC error \"XHASHTABLE_FOR_EACH is deprecated - use XHASHTABLE_FOR_EACH_SAFE instead\n\"") \
-	if (1) { \
-		(var) = NULL; \
-		goto CONCAT(label_, __LINE__); \
-	} \
-	else for (;;) \
-		if (1) break; \
-		else CONCAT(label_, __LINE__): \
-			for (int CONCAT(i_, __LINE__) = 0, CONCAT(j_, __LINE__); \
-				((var) == NULL || \
-				 XDLIST_FIELD(&(name)->hash_data.hash[CONCAT(j_, __LINE__)], var) == \
-				 &(name)->hash_data.hash[CONCAT(j_, __LINE__)].list_with_size.list) && \
-				 CONCAT(i_, __LINE__) < XHASHTABLE_N_BUCKETS(name); \
-				 CONCAT(j_, __LINE__) = CONCAT(i_, __LINE__), \
-				 ++CONCAT(i_, __LINE__)) \
-				 XDLIST_FOREACH(var, &(name)->hash_data.hash[CONCAT(i_, __LINE__)])
 #define XHASHTABLE_FOR_EACH_SAFE(var, name) \
 	if (1) { \
 		(var) = NULL; \
 		goto CONCAT(llabel_, __LINE__); \
-	} \
-	else for (;;) \
+	} else for (;;) \
 		if (1) break; \
 		else CONCAT(llabel_, __LINE__): \
 			for (int CONCAT(i_, __LINE__) = 0, CONCAT(j_, __LINE__); \
@@ -496,13 +373,8 @@ static inline uint32_t xhash_32(uint32_t val, uint32_t bits)
 				 ++CONCAT(i_, __LINE__)) \
 				 XDLIST_FOREACH_SAFE(var, &(name)->hash_data.hash[CONCAT(i_, __LINE__)])
 
-#define XHASHTABLE_FOR_EACH_POSSIBLE(var, name, key) \
-	XDLIST_FOREACH(var, &(name)->hash_data.hash[ \
-		XHASHTABLE_MIN(key, name)])
-
 #define XHASHTABLE_FOR_EACH_POSSIBLE_SAFE(var, name, key) \
-	XDLIST_FOREACH_SAFE(var, &(name)->hash_data.hash[ \
-		XHASHTABLE_MIN(key, name)])
+	XDLIST_FOREACH_SAFE(var, &(name)->hash_data.hash[XHASHTABLE_MIN(key, name)])
 
 /* Double Hash-table with 2-keys */
 /* declare a new double-hashtable */
@@ -569,8 +441,7 @@ static inline uint32_t xhash_32(uint32_t val, uint32_t bits)
 #define XDBL_HASHTABLE_N_ELEMENTS(name) ((name)->n_elements)
 
 /* check if hashtable has elements in any of the buckets */
-#define XDBL_HASHTABLE_EMPTY(name) \
-({ \
+#define XDBL_HASHTABLE_EMPTY(name) ({ \
 	bool CONCAT(f_, __LINE__) = true; \
 	for (int CONCAT(i_, __LINE__); \
 		CONCAT(i_, __LINE__) < XDBL_HASHTABLE_N_BUCKETS1(name); ++i); \
@@ -636,173 +507,4 @@ else for (;;) \
 	XDLIST_FOREACH_SAFE(var, &(name)->hash[ \
 	XHASHTABLE_MIN1(key1, name)][XHASHTABLE_MIN2(key2, name)])
 
-/* list and hashtable unitests */
-#if 0
-
-#include <stdio.h>
-#include <stdlib.h>
-
-struct my_data {
-	int x;
-	long y;
-	struct xdlist link;
-};
-
-#define LINE fprintf(stderr, "--- %d\n", __LINE__)
-
-int
-main(int a, char **b) {
-#if TEST_LIST
-	XDLIST_DECLARE(zzz, struct my_data, link) list = XDLIST_INIT(list);
-	struct my_data aa[2];
-	struct my_data *p;
-
-	for (int i = 0; i < 2; ++i) {
-		aa[i].x = i + 7;
-		aa[i].y = sqrt(aa[i].x);
-		XDLIST_ADD_TAIL(&list, &aa[i]);
-	}
-
-	LINE;
-
-	printf("XDLIST_FOREACH\n");
-	XDLIST_FOREACH(p, &list) {
-		printf("x=%d, y=%lf\n", p->x, p->y);
-	}
-	printf("XDLIST_FOREACH_REVERSE\n");
-	XDLIST_FOREACH_REVERSE(p, &list) {
-		printf("x=%d, y=%lf\n", p->x, p->y);
-	}
-	printf("XDLIST_FOREACH_SAFE\n");
-	XDLIST_FOREACH_SAFE(p, &list) {
-		printf("x=%d, y=%lf\n", p->x, p->y);
-		XDLIST_DEL(&p->link);
-	}
-	printf("XDLIST_FOREACH\n");
-	XDLIST_FOREACH(p, &list) {
-		printf("x=%d, y=%lf\n", p->x, p->y);
-	}
-
-	LINE;
-
-	for (int i = 0; i < 2; ++i) {
-		aa[i].x = i + 7;
-		aa[i].y = sqrt(aa[i].x);
-		XDLIST_ADD_TAIL(&list, &aa[i]);
-	}
-
-	printf("XDLIST_FOREACH\n");
-	XDLIST_FOREACH(p, &list) {
-		printf("x=%d, y=%lf\n", p->x, p->y);
-	}
-
-	printf("XDLIST_FOREACH_REVERSE\n");
-	XDLIST_FOREACH_REVERSE(p, &list) {
-		printf("x=%d, y=%lf\n", p->x, p->y);
-	}
-	printf("XDLIST_FOREACH_REVERSE_SAFE\n");
-	XDLIST_FOREACH_REVERSE_SAFE(p, &list) {
-		printf("x=%d, y=%lf\n", p->x, p->y);
-		XDLIST_DEL(&p->link);
-	}
-	printf("XDLIST_FOREACH_REVERSE\n");
-	XDLIST_FOREACH_REVERSE(p, &list) {
-		printf("x=%d, y=%lf\n", p->x, p->y);
-	}
-
-	LINE;
-
-	for (int i = 0; i < 2; ++i) {
-		aa[i].x = i + 7;
-		aa[i].y = sqrt(aa[i].x);
-		XDLIST_ADD_TAIL(&list, &aa[i]);
-	}
-
-	printf("XDLIST_FOREACH\n");
-	XDLIST_FOREACH(p, &list) {
-		printf("x=%d, y=%lf\n", p->x, p->y);
-	}
-
-	printf("XDLIST_DEL\n");
-	XDLIST_DEL(&aa[0].link);
-	printf("XDLIST_FOREACH\n");
-	XDLIST_FOREACH(p, &list) {
-		printf("x=%d, y=%lf\n", p->x, p->y);
-	}
-
-	printf("XDLIST_ELEM_DEL\n");
-	XDLIST_ELEM_DEL(&list, &aa[1]);
-	printf("XDLIST_FOREACH\n");
-	XDLIST_FOREACH(p, &list) {
-		printf("x=%d, y=%lf\n", p->x, p->y);
-	}
-
-	printf("XDLIST_FOREACH_REVERSE\n");
-	XDLIST_FOREACH_REVERSE(p, &list) {
-		printf("x=%d, y=%lf\n", p->x, p->y);
-	}
-	printf("XDLIST_FOREACH_REVERSE_SAFE\n");
-	XDLIST_FOREACH_REVERSE_SAFE(p, &list) {
-		printf("x=%d, y=%lf\n", p->x, p->y);
-		XDLIST_DEL(&p->link);
-	}
-	printf("XDLIST_FOREACH_REVERSE\n");
-	XDLIST_FOREACH_REVERSE(p, &list) {
-		printf("x=%d, y=%lf\n", p->x, p->y);
-	}
-#elif TEST_HASH
-	XHASHTABLE_DECLARE(, struct my_data, link, 5) hash;
-	XHASHTABLE_INIT(&hash);
-	struct my_data bb[64];
-	struct my_data *q;
-
-	for (int i = 0; i < ARRAY_SIZE(bb); ++i) {
-		bb[i].x = i + 7;
-		bb[i].y = bb[i].x * bb[i].x;
-		XHASHTABLE_ADD(&hash, &bb[i], (uint32_t)bb[i].y);
-	}
-#	if 1
-/* check the macros */
-	XHASHTABLE_FOR_EACH(q, &hash) {
-		printf("x=%d, y=%ld\n", q->x, q->y);
-		if (q->x == 13) {
-			break;
-		}
-	}
-
-#	else
-/* check the code using gcc -E */
-	LINE;
-if (1) {
-	(q) = ((void *)0);
-	goto label_362;
-}
-else for ( ; ; )
-	if (1) break;
-	else label_362: {
-		LINE;
-		printf("q=%p, array_size=%ld\n", q, ARRAY_SIZE((&hash)->hash));
-		for (int i_362 = 0, j;
-			  ((q) == ((void *)0) ||
-			   ((struct xdlist *) ((char *)(q) + sizeof(*(&(&hash)->hash[j])->data->offset))) == &(&hash)->hash[j].list) &&
-			   i_362 < ARRAY_SIZE((&hash)->hash); j = i_362, ++i_362) {
-			printf("i_362=%d\n", i_362);
-			for ((q) = ((typeof(((&(&hash)->hash[i_362]))->data->t)) ((char *)((&(&hash)->hash[i_362])->list.next) - sizeof(*((&(&hash)->hash[i_362]))->data->offset)));
-				  ((struct xdlist *) ((char *)((q)) + sizeof(*((&(&hash)->hash[i_362]))->data->offset))) != &(&(&hash)->hash[i_362])->list;
-				  (q) = ((typeof((((&(&hash)->hash[i_362])))->data->t)) ((char *)(((struct xdlist *) ((char *)(((q))) + sizeof(*(((&(&hash)->hash[i_362])))->data->offset)))->next) - sizeof(*(((&(&hash)->hash[i_362])))->data->offset)))) {
-				printf("x=%d, y=%ld\n", q->x, q->y);
-			}
-			printf("i_362=%d\n", i_362);
-		}
-	}
-
-
-
-#	endif
-#endif
-	return 0;
-}
-
-#endif
-
-#endif
+#endif	// NVMEIBT_DS
