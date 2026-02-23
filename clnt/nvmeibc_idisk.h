23e96e4e55429e6a743b276a9be219daa2394c8f
Roman Yakovenko <ryakovenko@nvidia.com>
2026-02-23 11:36:05 +0200

    [NVMESH-5707] [code rabbit] add nonnull attribute to nvmeibc_idisk
    Change-Id: I43faee168d020f68d5573b5121afb260db9d2e5a
    Signed-off-by: Roman Yakovenko <ryakovenko@nvidia.com>

--- clnt/nvmeibc_idisk.h
+++ clnt/nvmeibc_idisk.h
@@ -12,22 +12,38 @@ struct nvmeibc_disk_client_journal;
 
 struct nvmeibc_idisk { /* shared interface/contract between block & core */
 	struct {
+		__attribute__((nonnull(1)))
 		char const *(*get_name)(struct nvmeibc_idisk const *self);
+		__attribute__((nonnull(1)))
 		char const *(*get_full_name)(struct nvmeibc_idisk const *self);
+		__attribute__((nonnull(1)))
 		char const *(*get_host_name)(struct nvmeibc_idisk const *self);
+		__attribute__((nonnull(1)))
 		bool (*should_pause)(struct nvmeibc_idisk const *self);
+		__attribute__((nonnull(1)))
 		bool (*is_cont_preventors_waited_too_long)(struct nvmeibc_idisk const *self);
+		__attribute__((nonnull(1)))
 		int (*get_sector_shift)(struct nvmeibc_idisk const *self);
+		__attribute__((nonnull(1)))
 		int (*get_md_size)(struct nvmeibc_idisk const *self);
+		__attribute__((nonnull(1)))
 		int (*get_max_request_size_bytes)(struct nvmeibc_idisk const *self);
+		__attribute__((nonnull(1)))
 		bool (*is_access_local)(struct nvmeibc_idisk const *self);
+		__attribute__((nonnull(1)))
 		size_t (*get_min_gen_cmd_bb)(struct nvmeibc_idisk const *self);
+		__attribute__((nonnull(1)))
 		struct nvmeibc_disk_client_journal const *(*get_journal)(struct nvmeibc_idisk const *self);
 		/* for tests, will be removed in future */
+		__attribute__((nonnull(1)))
 		struct nvmeibc_disk_client_journal *(*get_journal_mut)(struct nvmeibc_idisk *self);
+		__attribute__((nonnull(1)))
 		int (*read_cont_preventors)(struct nvmeibc_idisk *self);
+		__attribute__((nonnull(1)))
 		int (*inc_cont_preventors)(struct nvmeibc_idisk *self);
+		__attribute__((nonnull(1)))
 		int (*dec_cont_preventors)(struct nvmeibc_idisk *self);
+		__attribute__((nonnull(1)))
 		enum nvmeibc_disk_status (*get_status)(struct nvmeibc_idisk const * self);
 	} ops;
 };
