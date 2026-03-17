0db0b274a76204fe16224905783127d71c193154
Yaniv Romem <yaniv@nvidia.com>
2026-03-17 10:50:32 +0200

    Add SPDX to the newer files.
    Change-Id: I8d2db6a508c36e2e3c1c41f2fa96ffc933e29ea1
    Signed-off-by: Yaniv <yaniv@nvidia.com>

--- tools/trace_daemon_2.0/unlink_list.h
+++ tools/trace_daemon_2.0/unlink_list.h
@@ -1,3 +1,8 @@
+/*
+* SPDX-FileCopyrightText: Copyright (c) 2026 NVIDIA CORPORATION & AFFILIATES. All rights reserved.
+* SPDX-License-Identifier: GPL-2.0-only OR Apache-2.0
+*/
+
 #ifndef UNLINK_LIST_H
 #define UNLINK_LIST_H
 
