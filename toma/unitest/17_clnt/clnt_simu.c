abbbd4dd53a5e194ab491a45d41ec2079521102d
danielhsh <DanielHe@nvidia.com>
2026-03-29 18:29:29 +0300

    NVMESH-7082: TomaSimu, Clnt simu, base IO
    Change-Id: I9aefbd62ec72cc047c8ed9d2c58ffdfa4716e1e1
    Signed-off-by: danielhsh <DanielHe@nvidia.com>

--- toma/unitest/17_clnt/clnt_simu.c
+++ toma/unitest/17_clnt/clnt_simu.c
@@ -1,6 +1,7 @@
 #include "clnt_simu.h"
 #include "nvmeibt_debug.h"	// Binary tracing
 #include "../13_mgmt/mongodb_simu.h"
+#include "common/nvmeib_shared.h"
 
 struct clnt_simu *clnt_simu_create(struct sb_cluster_conf *cfg, int node_idx) {
 	struct clnt_simu *C = calloc(1, sizeof(*C));
@@ -25,4 +26,38 @@ void clnt_simu_vol_attach(struct sb_cluster_conf *cfg, int node_idx, int vol_idx
 
 void clnt_simu_vol_detach(struct sb_cluster_conf *cfg, int node_idx, int vol_idx) {
 	cfg->vols[vol_idx].clnts[node_idx].attachment_version = 0;		// Todo...
+}
+
+void clnt_simu_vol_unregister(struct sb_cluster_conf *cfg, int node_idx, int vol_idx /*, int praid_idx*/) {
+	struct clnt_simu *C = cfg->nodes[node_idx].clnt;
+	struct clnt_praid_reg_ctx *reg = &C->regs[vol_idx+1000];	// Todo: properly extract from registered client
+	BUG_ON(reg->lock_id == 0); // All the acquired locks are abandoned and become stale
+	N_Tf(__AUTOID__, "Clnt=@X, unreg_lock=@LOCKID, leaving @INT stale locks" , cfg->nodes[node_idx].uuid, reg->lock_id, reg->n_ios);
+	reg->lock_id = 0;
+}
+
+#include "../sandbox_nvme.h"
+static void __simulate_io_to_disk(struct clnt_simu *C, struct sandbox_nvme_device *disk, u32 dlba_blockset) {
+	union nvmeib_lock_blkset_entry *ptr = &((union nvmeib_lock_blkset_entry *)disk->ram.addr)[dlba_blockset];
+	struct clnt_praid_reg_ctx *reg = &C->regs[1000];	// Todo: properly extract from registered client
+	BUG_ON((disk->ram.addr == NULL) || (disk->ram.len <= dlba_blockset));
+	ptr->lock_id.all = reg->lock_id;
+	ptr->blkset_info.bits.dirty = 0;		// Todo: inject dbits
+	ptr->blkset_info.bits.txid++;
+	reg->n_ios++;
+}
+
+void clnt_simu_vol_lock_blockset_v(struct sb_cluster_conf *cfg, int node_idx, int vol_idx, u32 vlba_blockset) {
+	const struct sb_attachment_info *A = &cfg->vols[vol_idx].clnts[node_idx];
+	struct clnt_simu *C = cfg->nodes[node_idx].clnt;
+	struct sandbox_nvme_device *disk = NULL;	// Todo: calculate from config
+	const u32 dlba_blockset = vlba_blockset+5;	// Todo: calculate vlba->dlba from config
+	BUG_ON(!A->ioEnabled);
+	__simulate_io_to_disk(C, disk, dlba_blockset);
+}
+
+void clnt_simu_vol_lock_blockset_d(struct sb_cluster_conf *cfg, int node_idx, int disk_idx, u32 dlba_blockset) {
+	struct clnt_simu *C = cfg->nodes[node_idx].clnt;
+	struct sandbox_nvme_device *disk = cfg->live->disks[disk_idx].local_nvme;
+	__simulate_io_to_disk(C, disk, dlba_blockset);
 }
--- toma/unitest/17_clnt/clnt_simu.h
+++ toma/unitest/17_clnt/clnt_simu.h
@@ -11,7 +11,10 @@ struct clnt_simu {
 	int node_idx;										// Node on which this client resides
 	struct clnt_praid_reg_ctx {
 		struct sb_praid_conf *ptr;						// The praid client is registering due to volume attach
+		u32 praid_version;								//
+		// Todo: Save minimal info about Toma topology to know which dbits to write
 		u32 lock_id;									// Lock id given by Toma, 0 if unregistered
+		u32 n_ios;										// N simulated ios to blocksets that were done
 	} regs[8];
 };
 
@@ -19,4 +22,7 @@ struct clnt_simu *clnt_simu_create(        struct sb_cluster_conf *, int node_id
 void              clnt_simu_destroy(       struct sb_cluster_conf *, int node_idx);
 struct clnt_simu *clnt_simu_get_local_clnt(struct sb_cluster_conf *);
 void clnt_simu_vol_attach(                 struct sb_cluster_conf *, int node_idx, int vol_idx);
+void clnt_simu_vol_lock_blockset_v(        struct sb_cluster_conf *, int node_idx, int vol_idx , u32 vlba_blockset);
+void clnt_simu_vol_lock_blockset_d(        struct sb_cluster_conf *, int node_idx, int disk_idx, u32 dlba_blockset);
+void clnt_simu_vol_unregister(             struct sb_cluster_conf *, int node_idx, int vol_idx /*, int praid_idx*/);		// Possibly leave stale lock
 void clnt_simu_vol_detach(                 struct sb_cluster_conf *, int node_idx, int vol_idx);
