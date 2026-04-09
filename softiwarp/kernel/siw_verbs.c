56f92967f7175270b925994fac69811078d14fd7
Jonathan Lemon <jlemon@nvidia.com>
2026-04-09 10:25:12 -0700

    [NVMESH-8018] Coderabbit complaint cleanups
    Change-Id: I1c88c1db4ca74ded387fb882cda150389171a7a3
    Signed-off-by: Jonathan Lemon <jlemon@nvidia.com>
    (cherry picked from commit 50cace3b7fe2999376fc6da194150acc3de4a962)
    
    Signed-off-by: Jonathan <jlemon@nvidia.com>

--- softiwarp/kernel/siw_verbs.c
+++ softiwarp/kernel/siw_verbs.c
@@ -1157,12 +1157,10 @@ struct ib_qp* siw_create_qp(struct ib_pd *ofa_pd,
 	qp->cpu = (smp_processor_id() + 1) % NR_CPUS;
 
 #ifdef USE_SQ_KTHREAD
-	if (scq) {
-		if (sdev->num_tx_vector > 0)
-			qp->cpu = sdev->tx_vector_cpu[scq->comp_vector % sdev->num_tx_vector];
-		else
-			qp->cpu = qp_tx_vector_cpu[scq->comp_vector % num_tx_vector];
-	}
+	if (sdev->num_tx_vector > 0)
+		qp->cpu = sdev->tx_vector_cpu[scq->comp_vector % sdev->num_tx_vector];
+	else
+		qp->cpu = qp_tx_vector_cpu[scq->comp_vector % num_tx_vector];
 #endif
 
 	dprint(DBG_OL, "<--\n");
