9b205a22aebeecd5ab94e99663f8ed0140a5c897
danielhsh <DanielHe@nvidia.com>
2026-04-13 07:58:55 +0300

    [NVMESH-7595] Fix (Toma) ammend2prev
    Change-Id: Ic7108f18259e5da14b69a77e8665c64021c2aac0
    Signed-off-by: danielhsh <DanielHe@nvidia.com>

--- toma/nvmeibt_global.c
+++ toma/nvmeibt_global.c
@@ -89,7 +89,6 @@ void nvmeibt_global_init(void)
 		"	N_CHUNKS="MACRO_DEF_TO_STR(NVMEIBT_MAX_N_CHUNKS)"\n"
 		"	N_CSV_LINES="MACRO_DEF_TO_STR(NVMEIBT_MAX_N_CSV_LINES)"\n"
 		"	N_CLIENTS_PER_NODE="MACRO_DEF_TO_STR(NVMEIBT_MAX_N_CLIENTS_PER_NODE)"\n"
-		"	N_CHUNKS="MACRO_DEF_TO_STR(NVMEIBT_MAX_N_CHUNKS)"\n"
 		"	N_PRAIDS="MACRO_DEF_TO_STR(NVMEIBT_MAX_N_PRAIDS)"\n"
 		"	N_SEGMENTS_IN_PRAID="MACRO_DEF_TO_STR(NVMEIBT_MAX_N_SEGMENTS_IN_PRAID)"\n"
 		"	N_PORTS_PER_NIC="MACRO_DEF_TO_STR(NVMEIBT_MAX_N_PORTS_PER_NIC)"\n"
@@ -907,7 +906,7 @@ void nvmeibt_global_idle_time_activities(void)
 	static int64_t				n_calls = 0;
 	const int64_t				n_calls_at_artificial_full_log = 10;
 	static int					last_kafka_idle_print_time_sec = 0;
-	int							kafka_idle_time_sec = nvmeibt_global_get_cur_event_start_time().tv_sec - nvmeibt_global_get_global()->kafka_last_activity_time.tv_sec;
+	const int					kafka_idle_time_sec = nvmeibt_global_get_cur_event_start_time().tv_sec - nvmeibt_global_get_global()->kafka_last_activity_time.tv_sec;
 
 	NFIN;
 	if (n_calls >= n_calls_at_artificial_full_log) {
@@ -922,7 +921,7 @@ void nvmeibt_global_idle_time_activities(void)
 		struct timespec now;
 		getnstimeofday(&now);
 		if (now.tv_sec - last_kafka_idle_print_time_sec > 30) { // print the error every 30 sec
-			N_ETf(i990kss, "Kafka has been idle for @INT sec", kafka_idle_time_sec);
+			N_ETf(i990kss, "Kafka client deadlock detected (for @INT[sec])! Consider manual restart...", kafka_idle_time_sec);
 			last_kafka_idle_print_time_sec = now.tv_sec;
 		}
 	}
