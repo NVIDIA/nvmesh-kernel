de0316093a6430d10021737ea8cefd86ff652f95
Cheng Li <cli1@nvidia.com>
2026-04-20 10:46:25 +0300

    [NVMESH-8517] define CORE_DBGDI_WR_MAX_MIRROR as N_MAX_RAID_MIRROR_SLICE_LEN by default
    Change-Id: I05a620c97cd1b6a01f52288b7b5280c7c815b31f
    Signed-off-by: Cheng Li <cli1@nvidia.com>

--- clnt/nvmeibc_core_dbgdi_blk.h
+++ clnt/nvmeibc_core_dbgdi_blk.h
@@ -30,7 +30,9 @@
 	(NVMEIBC_MAGIC_AREA_ATOMIC_SIZE_BYTES /                                    \
 	 sizeof(union t_core_magic_area_cell))
 
-#define CORE_DBGDI_WR_MAX_MIRROR 3
+/* Defined in common/nvmeib_shared.h; USER_SPACE kr_incs often omits that include. */
+#include "common/nvmeib_shared.h"
+#define CORE_DBGDI_WR_MAX_MIRROR N_MAX_RAID_MIRROR_SLICE_LEN
 
 struct t_core_dbgdi_magic_area {
 	union t_core_magic_area_cell a[NVMEIBC_CORE_DBG_DI_POSION_AREA_ARR_SIZE];
