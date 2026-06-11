8ccb0a53023fc54e723e79fc7ba3b09943726501
Jared Holzman <jholzman@nvidia.com>
2026-06-11 19:33:17 +0300

    [NVMESH-9238] fix (siw): Add SIW_BUG_ON_NON_PRODUCTION
    Change-Id: I80bc6da7eef05d284ef6a4fdd3b3547574c77bed
    Signed-off-by: Jared <jholzman@nvidia.com>

--- softiwarp/kernel/siw.h
+++ softiwarp/kernel/siw.h
@@ -1624,6 +1624,23 @@ static inline struct siw_mr *siw_mem2mr(struct siw_mem *m)
 		}							\
 	} while (0)
 
+#if defined(NVMESH_IS_PRODUCTION_COMPILATION) && (NVMESH_IS_PRODUCTION_COMPILATION==1)
+	#define SIW_BUG_NON_PRODUCTION(bug_num) WARN(1, NVMESH_BUG_PREFIX_FMT, bug_num)
+	#define SIW_BUG_ON_NON_PRODUCTION(cond, bug_num) WARN_ON(cond, NVMESH_BUG_PREFIX_FMT, bug_num)
+#else
+	#define SIW_BUG_NON_PRODUCTION(bug_num) do { \
+		pr_err("KERNEL WARNING TRIGGERED AT %s:%d - Bug Number: %d, BUG_ON for debug", __FILE__, __LINE__, bug_num); \
+		BUG();\
+	} while (0)
+
+	#define SIW_BUG_ON_NON_PRODUCTION(cond, bug_num) do { \
+		if (cond) { \
+			pr_err("KERNEL WARNING TRIGGERED AT %s:%d - Bug Number: %d, BUG_ON for debug", __FILE__, __LINE__, bug_num); \
+			BUG(); \
+		} \
+	} while (0)
+#endif
+
 #include "../../common/compat/kr_incs_types.h"
 
 #endif
